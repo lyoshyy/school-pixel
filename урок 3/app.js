d5e177f3bcff28fb1739de131ac0e9727c8508b8
lyosha <lyoha.denisov98@yahoo.com>
2024-02-05 06:00:31 +0300

    learn

---
+++ урок 3/app.js
@@ -0,0 +1,13 @@
+const titleProject = "Pixel";
+const screensValue = "шаблонные, с уникальным дизайном, с анимациями";
+const screenPrice = 1000;
+const percentage = 7;
+const fullPrice = 5000n;
+const responsive = true;
+
+console.log("Название проекта:", titleProject, 'Оценка стоимости:', screenPrice % percentage);
+console.log("Тип экранов:", screensValue);
+console.log("Цена реализации одного экрана:", screenPrice);
+console.log("Процент отката подрядчику:", percentage);
+console.log("Желаемая сумма заработка с проекта:", fullPrice);
+console.log(responsive);
